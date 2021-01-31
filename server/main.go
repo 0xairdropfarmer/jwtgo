6127c518eb128519ad12ad0e404d0ab859ffe8d1
krissanawat101 <krissanawat101@gmail.com>
2021-01-31 17:00:32 +0700

    near finish

--- server/main.go
+++ server/main.go
@@ -16,19 +16,36 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header["Token"] != nil {
+
 			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
 				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf(("There was an error"))
+					return nil, fmt.Errorf(("Invalid Signing Method"))
 				}
+				if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+					return nil, fmt.Errorf(("Expire token"))
+				}
+				aud := "billing.jwtgo.io"
+				checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+				if !checkAudience {
+					return nil, fmt.Errorf(("invalid aud"))
+				}
+				// verify iss claim
+				iss := "jwtgo.io"
+				checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+				if !checkIss {
+					return nil, fmt.Errorf(("invalid iss"))
+				}
+
 				return MySigningKey, nil
 			})
 			if err != nil {
 				fmt.Fprintf(w, err.Error())
 			}
+
 			if token.Valid {
 				endpoint(w, r)
 			}
-			fmt.Println(token)
+
 		} else {
 			fmt.Fprintf(w, "No Authorization Token provided")
 		}
